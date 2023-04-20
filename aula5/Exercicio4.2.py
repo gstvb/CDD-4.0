7a6b2e17b8235e4fe95134985bb75ccc0fa67b43
Gustavo Borges <99938149+gstvb@users.noreply.github.com>
2023-04-20 10:39:41 -0300

    Add files via upload

---
+++ aula5/Exercicio4.2.py
@@ -0,0 +1,28 @@
+resp = 0
+
+while resp !=6:
+    num1= int(input("Digite o primeiro número: "))
+    num2= int(input("Digite o segundo número: "))
+
+    while True:
+        resp = int(input("Digite 1 para somar \n"
+                             "Digite 2 para Subtrair \n"""
+                             "Digite 3 para Multiplicar\n"
+                             "Digite 4 para Dividir\n"
+                             "Digite 5 para Digitar novo número \n"
+                             "Digite 6 para Para sair\n"))
+        match resp:
+            case 1:
+                print(num1+num2)
+            case 2:
+                print(num1-num2)
+            case 3:
+                print(num1*num2)
+            case 4:
+                print(num1/num2)
+            case 5:
+                break
+
+            case 6:
+                print("Programa encerrado.")
+
---
+++ aula5/Exercicio4.py
@@ -0,0 +1,38 @@
+opcao = 0
+soma = 0
+subtração = 0
+multiplicacao = 0
+divisao = 0
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+print("Selecione a operação: ")
+print(" 1. Para soma","\n","2. Para subtração","\n","3. Para multiplicação","\n","4. Para divisão","\n","5. Para digitar novo número","\n","6. Para sair")
+opcao = int(input("Digite a opção: "))
+
+while opcao == 5:
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+
+    print("Selecione a operação: ")
+    print(" 1. Para soma","\n","2. Para subtração","\n","3. Para multiplicação","\n","4. Para divisão","\n","5. Para digitar novo número","\n","6. Para sair")
+    opcao = int(input("Digite a opção: "))
+
+    while opcao != 6:
+        if opcao == 1:
+            soma = num1 + num2
+            print(soma)
+        elif opcao == 2:
+            subtração = num1 - num2
+            print(subtração)
+        elif opcao == 3:
+            multiplicacao = num1 * num2
+            print(multiplicacao)
+        elif opcao == 4:
+            divisao = num1 / num2
+            print(divisao)
+    else:
+            print("Programa encerrado.")
+
+
