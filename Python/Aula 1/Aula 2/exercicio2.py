10f0dfd0f1a03dce17403c3a7235e342617770b3
Gustavo Borges <99938149+gstvb@users.noreply.github.com>
2023-05-12 12:02:44 -0300

    Add files via upload

---
+++ Python/Aula 1/Aula 2/exercicio2.py
@@ -0,0 +1,10 @@
+"""
+from Python.funcoes import analisa_texto
+
+retorno = analisa_texto(texto="O rato roeu a roupa do rei de Roma.")
+print(retorno)
+"""
+from Python.funcoes import texto
+
+retorno = texto("Boa noite!")
+print(retorno)
---
+++ Python/Aula 1/Aula 2/exertcicio3.py
@@ -0,0 +1,4 @@
+from Python.funcoes import listas
+
+retorno = listas([2, 4, 4, 5, 6, 8, 8, 8, 9, 10])
+print(retorno)
