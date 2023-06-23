8a2cebad6c8dd7b911a0d04628335b7ece92a0d8
Gustavo Borges <99938149+gstvb@users.noreply.github.com>
2023-06-23 01:52:00 -0300

    Add files via upload

---
+++ Lógica de Programação/aula5/exercicio5.py
@@ -0,0 +1,19 @@
+"""
+for c in range (1,11,1):
+    print(c,end=" ")
+
+for x in range (10,0,-1):
+    print(x, end=" ")
+"""
+"""
+cont = 1
+
+while (cont < 11):
+    print(cont, end=" ")
+    cont = cont + 1
+"""
+cont = 10
+
+while (cont >= 1):
+    print(cont, end=" ")
+    cont = cont - 1
