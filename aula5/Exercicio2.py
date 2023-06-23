00930d881657730620c1cbf15e1c34004af83c35
Gustavo Borges <99938149+gstvb@users.noreply.github.com>
2023-06-23 01:52:58 -0300

    Delete aula5 directory

--- aula5/Exercicio2.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Digite um número inteiro: "))
-for n in range(num+1):
-    for y in range(n):
-        print(n, end=" ")
-
-    print()
--- aula5/Exercicio3.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Digite um número inteiro: "))
-for n in range(num+2):
-    for y in range(n):
-        print(y, end=" ")
-    print()
