272678197be64f669ed4c8c1c56955d089bd8e2f
Gustavo Borges <99938149+gstvb@users.noreply.github.com>
2023-06-23 01:51:01 -0300

    Add files via upload

---
+++ Lógica de Programação/aula5/exercicio2.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número inteiro: "))
+for n in range(num+1):
+    for y in range(n):
+        print(n, end=" ")
+
+    print()
---
+++ Lógica de Programação/aula5/exercicio3.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número inteiro: "))
+for n in range(num+2):
+    for y in range(n):
+        print(y, end=" ")
+    print()
