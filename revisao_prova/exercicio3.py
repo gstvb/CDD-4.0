03b549eb2d3d267d5301fa60ebea7f971386cefe
Gustavo Borges <99938149+gstvb@users.noreply.github.com>
2023-06-23 01:19:12 -0300

    Delete revisao_prova directory

--- revisao_prova/exercicio3.py
+++
@@ -1,4 +0,0 @@
-idade = int(input("Digite sua idade: "))
-
-ano = 2023 - idade
-print(f"Ano de nascimento: {ano}")
--- revisao_prova/exercicio9.py
+++
@@ -1,8 +0,0 @@
-num = int(input('Digite um número inteiro: '))
-
-if num > 0:
-    sub = num - 1
-    print(f"Antececor: {sub}")
-else:
-    soma = num + 1
-    print(f"Antececor: {soma}")
